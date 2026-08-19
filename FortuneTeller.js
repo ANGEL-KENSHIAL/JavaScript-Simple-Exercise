1820be3384360cd04696786a761bdb6501c518dd
ANGEL WORLD <kenshial07@gmail.com>
2026-08-19 20:57:07 +0000

    I changed the const key word to let and then updated the document.getElementById() into simple variable naming which the values are assigned directly from there.

--- FortuneTeller.js
+++ FortuneTeller.js
@@ -1,8 +1,8 @@
 function tellFortune() {
-  const numChildren = document.getElementById("numChildren").value;
-  const partnerName = document.getElementById("partnerName").value;
-  const location = document.getElementById("location").value;
-  const jobTitle = document.getElementById("jobTitle").value;
+  let numChildren = 2;
+  let partnerName = "Angel Kenshial";
+  let location = "Austria";
+  let jobTitle = "Full Stack Developer";
 
   console.log(
     `You will be a ${jobTitle} in ${location}, and married to ${partnerName} with ${numChildren} kids.`,
